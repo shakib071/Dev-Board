7c8f868ed56e4d741c84acb9de973a718aa7adb4
shakib071 <shakibhasan071@gmail.com>
2025-03-02 02:09:36 +0600

    clear history section

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById('discover').addEventListener('click',
     function () {
         window.location.href = 'blog.html';
     }
-)
+);
 
 
 
@@ -17,3 +17,13 @@ cardBtn('card-btn3');
 cardBtn('card-btn4');
 cardBtn('card-btn5');
 cardBtn('card-btn6');
+
+
+// clear history section
+
+document.getElementById('clear-history').addEventListener('click',
+    function (event){
+        event.preventDefault();
+        document.getElementById('history').innerHTML = '';
+    }
+);
