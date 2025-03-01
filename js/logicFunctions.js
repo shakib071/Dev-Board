69fc637bb86562a5c80f8c497d42d2bd1d423595
shakib071 <shakibhasan071@gmail.com>
2025-03-01 19:19:36 +0600

    card

--- js/logicFunctions.js
+++ js/logicFunctions.js
@@ -9,16 +9,40 @@ function cardBtn(id) {
 
         event.preventDefault();//prevent the default action of the form ie default reload
     
+        //task assaign section update
         let taskAssaign = document.getElementById('task_assigned');
-        console.log(taskAssaign);
-    
+        
         let taskAssaignValueInt = parseInt(taskAssaign.innerText);
-        console.log(taskAssaignValueInt);
+        
     
         taskAssaignValueInt -= 1;
-        console.log(taskAssaignValueInt);
-    
         taskAssaign.innerText = taskAssaignValueInt;
+
+        //task completed section update nav section
+
+        let taskCompleted = document.getElementById('task_completed');
+
+        let taskCompletedValueInt = parseInt(taskCompleted.innerText);
+        taskCompletedValueInt += 1;
+        taskCompleted.innerText = taskCompletedValueInt;
+        
+
+        // History section update
+
+        //parent div
+            let historyDiv = document.getElementById('history');
+            console.log(historyDiv);
+
+        //child paragraph 
+            let historyChild = document.createElement('p');
+            historyChild.classList.add('mb-5', 'bg-[#F4F7FF]', 'rounded-xl' , 'p-2')
+            
+            historyChild.innerText = 'You have Complete The Task Add Dark Mode at 12:48:15 PM';
+            historyDiv.appendChild(historyChild);
+
+
+
+
         
     
     });
